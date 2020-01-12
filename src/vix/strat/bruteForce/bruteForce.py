7b46be65af707442b7b88eff8e3dc9cef7dc0059
Eeee569 <willwolfe1@gmail.com>
2020-01-12 07:11:51 -0600

    addded old vix program

---
+++ src/vix/strat/bruteForce/bruteForce.py
@@ -0,0 +1,25 @@
+
+
+#
+# ind = 0
+#
+# def convertDate(x):
+#         ind+=1
+#         return ind
+#
+#
+#
+# new = df['Time'].apply(convertDate)
+# print(new.head())
+
+#
+# def printData(x):
+#     if x.name=='Time':
+#         print(str(x))
+#
+#
+# df['Time']=df['Time'].apply(str)
+#
+# df['Last']=df['Last'].apply(float)
+#
+# print(df.dtypes)
