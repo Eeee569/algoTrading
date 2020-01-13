0e64d8ebb6d53c3b17373410e963000d07ca0f24
Eeee569 <willwolfe1@gmail.com>
2020-01-12 19:18:19 -0600

    started bare bones brute force for vix

--- src/vix/strat/bruteForce/bruteForce.py
+++ src/vix/strat/bruteForce/bruteForce.py
@@ -1,25 +1,40 @@
+import pandas as pd
+from multiprocessing import Pool
+import numpy as np
+
+CSV_LOCATION = "../../vixData/cleanVixData.csv"
+MULTIPLIER = 100
+TIME_INTERVAL = 5
+INCRAMENT = 0.1 * MULTIPLIER 
+B_PRIME = 0.4 * MULTIPLIER 
 
 
-#
-# ind = 0
-#
-# def convertDate(x):
-#         ind+=1
-#         return ind
-#
-#
-#
-# new = df['Time'].apply(convertDate)
-# print(new.head())
+def main():
 
-#
-# def printData(x):
-#     if x.name=='Time':
-#         print(str(x))
-#
-#
-# df['Time']=df['Time'].apply(str)
-#
-# df['Last']=df['Last'].apply(float)
-#
-# print(df.dtypes)
+	##DATA CLEANING
+	df = pd.read_csv(CSV_LOCATION)
+	#set the interval to 5 minutes 
+	df = df.iloc[::TIME_INTERVAL, :]
+	#remove the extra unnamed column
+	df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+	df['Last'] = df['Last'].apply(lambda x: x*MULTIPLIER)
+	df['Last']= df['Last'].astype(str).astype(int)
+	print(df.head())
+	max_vix = df['Last'].max()
+	min_vix = df['Last'].min()
+
+	for a in np.arange(min_vix - (INCRAMENT*2), max_vix , INCRAMENT):
+		for b in np.arange(min_vix - (INCRAMENT*1), max_vix - (INCRAMENT*1), INCRAMENT):
+			for c in np.arange(min_vix, max_vix - (INCRAMENT*2) , INCRAMENT):
+				print("a: "+str(a)+" b: "+str(b)+" c: " +str(c))
+
+#def run_test(a,b,c,df)
+	
+	
+
+
+
+if __name__ == '__main__':
+	main()
+
+
