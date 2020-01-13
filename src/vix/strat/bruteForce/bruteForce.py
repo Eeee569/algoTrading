12730b222003585e6b070f6408c947ec438dc205
Eeee569 <willwolfe1@gmail.com>
2020-01-12 23:33:05 -0600

    added string builder

--- src/vix/strat/bruteForce/bruteForce.py
+++ src/vix/strat/bruteForce/bruteForce.py
@@ -34,10 +34,12 @@ def main():
 	for a in np.arange(min_vix , max_vix - (INCRAMENT*2) , INCRAMENT):
 		for b in np.arange(a + (INCRAMENT*1), max_vix - (INCRAMENT*1), INCRAMENT):
 			for c in np.arange(b + (INCRAMENT*1), max_vix , INCRAMENT):
+
 				proc = Process(target=run_test, args=(a,b,c,df.copy())) #df.copy() may not be nesisary
 				procs.append(proc)
 				proc.start()
 				proc.join(timeout=0)
+
 				while(len(procs)>=NUM_PROC):					
 					time.sleep(1)
 					i=0
@@ -54,28 +56,40 @@ def main():
 #	for p in procs:
 #		p.join()
 
+def build_string(a,b,c,df):
+
+	prev=-1
+	string_list=[]
+	for index, row in df.iterrows():
+
+		#need testing, this is also only for values going up
+
+		if(a>prev and a<=row['Last']):
+			string_list.append('a')
+		elif((b-B_PRIME)>prev and(b-B_PRIME)<=row['Last']):
+			string_list.append('b`')
+		elif(b>prev and b<=row['Last']):
+			string_list.append('b')
+		elif(c>prev and c<=row['Last']):
+			string_list.append('c')
+		
+		prev=row['Last']
+
+	return string_list
+
 
 def run_test(a,b,c,df):
 	
-	print("a: "+str(a)+" b: "+str(b)+" c: " +str(c))
+	string_list=build_string(a,b,c,df)
+
+	for i in string_list:
+		print(i)
 
 
 
 
-class Stop_Queue:
-	def __init__(self):
-		self.q=queue.Queue(maxsize=16) 
-		self.size = 0
-	def put(self,proc):
-		if(not q.full()):
-			q.put(proc)
-			self.size +=1
-			return True
-		else:
-			return False
-	def pop(self):
-		if(self.size<=0):
-			return False; 
+
+
 
 
 if __name__ == '__main__':
