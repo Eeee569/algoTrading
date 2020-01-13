d9bac81bbf3d60f02935945053293d6dbcc84c11
Eeee569 <willwolfe1@gmail.com>
2020-01-12 20:37:47 -0600

    added basic multithreading

--- src/vix/strat/bruteForce/bruteForce.py
+++ src/vix/strat/bruteForce/bruteForce.py
@@ -5,8 +5,8 @@ import numpy as np
 CSV_LOCATION = "../../vixData/cleanVixData.csv"
 MULTIPLIER = 100
 TIME_INTERVAL = 5
-INCRAMENT = 0.1 * MULTIPLIER 
-B_PRIME = 0.4 * MULTIPLIER 
+INCRAMENT = int(0.1 * MULTIPLIER) 
+B_PRIME = int(0.4 * MULTIPLIER )
 
 
 def main():
@@ -18,19 +18,30 @@ def main():
 	#remove the extra unnamed column
 	df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 	df['Last'] = df['Last'].apply(lambda x: x*MULTIPLIER)
-	df['Last']= df['Last'].astype(str).astype(int)
+	#set df to int
+	df['Last']= df['Last'].astype(np.int64)
 	print(df.head())
 	max_vix = df['Last'].max()
 	min_vix = df['Last'].min()
 
-	for a in np.arange(min_vix - (INCRAMENT*2), max_vix , INCRAMENT):
-		for b in np.arange(min_vix - (INCRAMENT*1), max_vix - (INCRAMENT*1), INCRAMENT):
-			for c in np.arange(min_vix, max_vix - (INCRAMENT*2) , INCRAMENT):
-				print("a: "+str(a)+" b: "+str(b)+" c: " +str(c))
+	procs=[]
+	pool = Pool(processes=8)
 
-#def run_test(a,b,c,df)
+	for a in np.arange(min_vix , max_vix - (INCRAMENT*2) , INCRAMENT):
+		for b in np.arange(a + (INCRAMENT*1), max_vix - (INCRAMENT*1), INCRAMENT):
+			for c in np.arange(b + (INCRAMENT*1), max_vix , INCRAMENT):
+				pool.apply_async(run_test, args=(a,b,c,df.copy(),)) #df.copy() may not be nesisary
+
+				#print("a: "+str(a)+" b: "+str(b)+" c: " +str(c))
+
+	#pool.close()
+	pool.join()
+
+
+def run_test(a,b,c,df):
 	
-	
+	print("a: "+str(a)+" b: "+str(b)+" c: " +str(c))
+
 
 
 
